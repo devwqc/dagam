128391ca2c237d38028166735012b3265f95866b
devwqc <devwqc@gmail.com>
2025-12-01 17:39:50 +0900

    refactor: supabase client DB 타입 제네릭 추가

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,6 +1,7 @@
 import envConfig from '@/constants/env-config';
 import { createBrowserClient } from '@supabase/ssr';
+import type { Database } from './database.types';
 
 export function createSupabaseBrowserClient() {
-  return createBrowserClient(envConfig.supabaseUrl, envConfig.supabasePublishableKey);
+  return createBrowserClient<Database>(envConfig.supabaseUrl, envConfig.supabasePublishableKey);
 }
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,11 +1,12 @@
 import envConfig from '@/constants/env-config';
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
+import type { Database } from './database.types';
 
 export async function createSupabaseServerClient() {
   const cookieStore = await cookies();
 
-  return createServerClient(envConfig.supabaseUrl, envConfig.supabasePublishableKey, {
+  return createServerClient<Database>(envConfig.supabaseUrl, envConfig.supabasePublishableKey, {
     cookies: {
       getAll() {
         return cookieStore.getAll();
